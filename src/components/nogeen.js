c2581ca6ad7032f199f45d1cce37bc599d672eb3
“Jordi <jordiklavers@gmail.com>
2026-01-29 22:12:13 +0100

    create item

--- src/components/nogeen.js
+++ src/components/nogeen.js
@@ -1,1 +1,17 @@
 console.log("nog een test component")
+
+function createItem(item, itemName) {
+    return {
+        name: itemName,
+        description: "Item description",
+        price: 100
+    }
+}
+
+let buttons = document.querySelectorAll("button")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        createItem(button.dataset.item, button.dataset.itemName); 
+    })
+})
